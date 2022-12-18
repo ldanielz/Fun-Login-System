8ca850bd0f27ad36efacb35f11bc429e5cb89599
Lizandro Daniel <ldaniel.eng@gmail.com>
2022-12-18 19:51:32 -0300

    Commit 02: Refactor Code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ import { defaultTheme } from '../styles/themes/default'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <ThemeProvider theme={defaultTheme}>
-    <Component {...pageProps} />
-  </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
-  }
+}
